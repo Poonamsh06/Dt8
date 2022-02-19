1a5ee4687e8a8c6c6c7569adfb026c6ff4a1ce82
hritikranjannanda <hritiknanda5099@gmail.com>
2022-02-19 15:00:03 +0530

    first commit on new web design

---
+++ lib/Pages/Topbar_items.dart
@@ -0,0 +1,28 @@
+class TopBarItem{
+  final String name,image,tap;
+  TopBarItem({required this.image, required this.name, required this.tap});
+}
+
+final List<TopBarItem> topBarItem = [
+  TopBarItem(
+      image:"images/calander.png",
+      name:'Calender',
+      tap:'calender',
+  ),
+  TopBarItem(
+    image:"images/rituals.jpg",
+    name:'Pujan Vidhi',
+    tap:'pujanvidhi',
+  ),
+  TopBarItem(
+    image:"images/f.png",
+    name:'Facts/Article',
+    tap:'article',
+  ),
+  TopBarItem(
+    image:"images/m.png",
+    name:'Muhurat',
+    tap:'muhurat',
+  )
+
+];
